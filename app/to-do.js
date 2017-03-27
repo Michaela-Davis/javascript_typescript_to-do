5682a95aac96ab8da9b9cab97f3af9bbe9657514
Michaela-Davis <michaela.delight@gmail.com>
2017-03-26 17:58:03 -0700

    declare Class method markDone

--- app/to-do.js
+++ app/to-do.js
@@ -1,15 +1,28 @@
+// CLASS
 var Task = (function () {
+    // CONSTRUCTOR
     function Task(descriptionParameter, priorityParameter) {
         this.descriptionParameter = descriptionParameter;
         this.priorityParameter = priorityParameter;
     }
+    // CLASS METHODS
+    Task.prototype.markDone = function () {
+        this.done = true;
+    };
     return Task;
 }());
 var tasks = [];
 tasks.push(new Task('Clean the litterboxes.', 'Medium'));
 tasks.push(new Task('Buy groceries.', 'Low'));
 tasks.push(new Task('Do laundry', 'High'));
+tasks[0].markDone();
+// Looping in TypeScript is:
+// for(var individualThing of listOfMultipleThings){
+//   whateverYouWantToDoWithLoop(thing);
+// };
+// LOOPING
 for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
     var task = tasks_1[_i];
     console.log(task);
 }
+;
--- app/to-do.ts
+++ app/to-do.ts
@@ -1,7 +1,14 @@
+// CLASS
 class Task {
   done: boolean;
 
+// CONSTRUCTOR
   constructor(public descriptionParameter: string, public priorityParameter: string){}
+
+// CLASS METHODS
+  markDone(){
+    this.done = true;
+  }
 }
 
 var tasks: Task[] = [];
@@ -9,11 +16,14 @@ tasks.push(new Task ('Clean the litterboxes.', 'Medium'));
 tasks.push(new Task ('Buy groceries.', 'Low'));
 tasks.push(new Task ('Do laundry', 'High'));
 
+tasks[0].markDone();
+
 // Looping in TypeScript is:
   // for(var individualThing of listOfMultipleThings){
   //   whateverYouWantToDoWithLoop(thing);
   // };
 
+// LOOPING
 for(var task of tasks){
   console.log(task);
 };
